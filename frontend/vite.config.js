3ed12660070ac23c5a304fa1aab5118656de5330
henok <anonsurf28@gmail.com>
2024-04-27 12:19:06 -0700

    chaged port

---
+++ frontend/vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+export default {
+    server: {
+      host: '0.0.0.0', // This allows Vite to listen on all network interfaces
+      port: 3000, // You can set the port number as per your requirement
+    },
+  };
+  
